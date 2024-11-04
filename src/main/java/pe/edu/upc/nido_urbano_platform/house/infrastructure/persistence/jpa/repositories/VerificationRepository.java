b2d9faa03bdb0fa424b415a1170ffb248e589df5
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-04 01:38:07 -0500

    feat(verification):  update verification repository.

--- src/main/java/pe/edu/upc/nido_urbano_platform/house/infrastructure/persistence/jpa/repositories/VerificationRepository.java
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house/infrastructure/persistence/jpa/repositories/VerificationRepository.java
@@ -1,10 +1,11 @@
 package pe.edu.upc.nido_urbano_platform.house.infrastructure.persistence.jpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.nido_urbano_platform.house.domain.model.entities.Verification;
 
 import java.util.Optional;
-
+@Repository
 public interface VerificationRepository extends JpaRepository<Verification,Long> {
     boolean existsByUniqueRegistrationNumber(String uniqueRegistrationNumber);
     Optional<Verification> findByVerificationId(Verification verificationId);
