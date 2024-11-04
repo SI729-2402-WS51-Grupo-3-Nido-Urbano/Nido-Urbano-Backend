7272abaf2cfa34b6c7a850a98734138681548ded
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-03 23:02:02 -0500

    feat(house): adds house attribute validation.

---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house/domain/model/valueobjects/HouseModal.java
@@ -0,0 +1,6 @@
+package pe.edu.upc.nido_urbano_platform.house.domain.model.valueobjects;
+
+public enum HouseModal {
+    SALE,
+    RENTAL
+}
---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house/domain/model/valueobjects/StatusLandlord.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.nido_urbano_platform.house.domain.model.valueobjects;
+
+public enum StatusLandlord {
+    NO_REQUEST,
+    IN_PROGRESS,
+    APPROVED
+}
