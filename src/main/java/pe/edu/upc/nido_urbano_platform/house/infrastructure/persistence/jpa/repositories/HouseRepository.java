259ef288f5ebe227eebc7dcd524ca6d28bf317fc
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-04 01:39:03 -0500

    feat(house): add house repository.

---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house/infrastructure/persistence/jpa/repositories/HouseRepository.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.nido_urbano_platform.house.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.nido_urbano_platform.house.domain.model.aggregates.House;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HouseRepository extends JpaRepository<House,Long> {
+    boolean existsByAddressAndHouseTypeAndHouseModal(String address, String houseType, String houseModal);
+    List<House> findByHouseModal(String houseModal);
+    List<House> findByAddress(String address);
+}
