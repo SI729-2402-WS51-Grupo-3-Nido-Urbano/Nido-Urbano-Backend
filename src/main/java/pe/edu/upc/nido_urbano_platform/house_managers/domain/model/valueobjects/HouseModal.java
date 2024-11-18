cbfc2f8749f207ce7081fb034bd1898c3c2e2d4e
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-18 17:36:04 -0500

    feat(house-manager): add value objects.

---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house_managers/domain/model/valueobjects/HouseModal.java
@@ -0,0 +1,6 @@
+package pe.edu.upc.nido_urbano_platform.house_managers.domain.model.valueobjects;
+
+public enum HouseModal {
+    SALE,
+    RENTAL
+}
---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house_managers/domain/model/valueobjects/StatusLandlord.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.nido_urbano_platform.house_managers.domain.model.valueobjects;
+
+public enum StatusLandlord {
+    NO_REQUEST,
+    IN_PROGRESS,
+    APPROVED
+}
