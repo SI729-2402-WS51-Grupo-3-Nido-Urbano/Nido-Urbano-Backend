de11018fc24321d1ce039f459eacb4131e0d86d2
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-18 17:40:31 -0500

    feat(house-managers): add queries get.

---
+++ src/main/java/pe/edu/upc/nido_urbano_platform/house_managers/domain/model/queries/GetHouseByHouseModalQuery.java
@@ -0,0 +1,6 @@
+package pe.edu.upc.nido_urbano_platform.house_managers.domain.model.queries;
+
+import pe.edu.upc.nido_urbano_platform.house_managers.domain.model.valueobjects.HouseModal;
+
+public record GetHouseByHouseModalQuery(String houseModal) {
+}
