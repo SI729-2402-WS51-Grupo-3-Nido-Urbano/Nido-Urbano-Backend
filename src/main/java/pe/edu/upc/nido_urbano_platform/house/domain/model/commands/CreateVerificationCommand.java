9ed4fb94e1530104816b5d2cd471efdea54712c6
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-18 17:33:42 -0500

    feat(house): deleted verification.

--- src/main/java/pe/edu/upc/nido_urbano_platform/house/domain/model/commands/CreateVerificationCommand.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.nido_urbano_platform.house.domain.model.commands;
-
-public record CreateVerificationCommand(
-        String ownerName,
-        String condition,
-        String uniqueRegistrationNumber
-) {
-}
--- src/main/java/pe/edu/upc/nido_urbano_platform/house/domain/model/commands/UpdateVerificationCommand.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.nido_urbano_platform.house.domain.model.commands;
-
-public record UpdateVerificationCommand(
-        Long verificationId,
-        String ownerName,
-        String condition,
-        String uniqueRegistrationNumber
-) {
-}
