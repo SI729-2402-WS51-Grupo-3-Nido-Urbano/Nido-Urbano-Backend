b791f44e498b9754f923b9aa7f22f289dc27908c
Rodrigo Chirinos <u202217804@upc.edu.pe>
2024-11-18 17:45:08 -0500

    fix: Improve controller functionality

--- src/main/java/pe/edu/upc/nido_urbano_platform/feedback/domain/model/valueobjects/Score.java
+++ src/main/java/pe/edu/upc/nido_urbano_platform/feedback/domain/model/valueobjects/Score.java
@@ -3,7 +3,7 @@ package pe.edu.upc.nido_urbano_platform.feedback.domain.model.valueobjects;
 public record Score(int value) {
 
     public Score {
-        if(value <= 1 || value > 5) {
+        if(value < 1 || value > 5) {
             throw new IllegalArgumentException("Feedback score has to be between 1 and 5");
         }
     }
